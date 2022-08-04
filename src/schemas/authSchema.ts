86297475c33795a971e5b7c8285cbd8ea81357e2
isabellacgmsa <isabellacgmsa1@gmail.com>
2022-08-04 18:26:11 -0300

    Preparando revisão de código

--- src/schemas/authSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-import { AuthData } from "../services/authService.js";
-
-const authSchema = Joi.object<AuthData>({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(10).required()
-});
-
-export default authSchema;
--- src/schemas/safenoteSchema.ts
+++
@@ -1,9 +0,0 @@
-import Joi from "joi";
-import { InputSafenoteData } from "../controllers/safenoteController.js";
-
-const safenoteSchema = Joi.object<InputSafenoteData>({
-  note: Joi.string().max(1000).required(),
-  title: Joi.string().max(50).required()
-});
-
-export default safenoteSchema;
